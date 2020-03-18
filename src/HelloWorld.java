890dbc8d2e2def342aa7443ba16209c88a8150b1
p <p@LAPTOP-EE19NQVS>
2020-03-18 20:57:11 +0900

    create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("** Clothes Management System Menu**");
+		System.out.println(" 1, Add clothes");
+		System.out.println(" 2, Delect clothes");
+		System.out.println(" 3, Edit clothes");
+		System.out.println(" 4, View clothes");
+		System.out.println(" 5, Exit");
+		System.out.println("select one number between 1~5");
+	}
+
+}
